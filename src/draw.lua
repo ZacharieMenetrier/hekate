8e93c7601e3da2715adae97b98b3501b30fa8237
Quentin FERRE <quentin.ferre@laposte.net>
2018-04-27 03:10:18 +0200

    Draw actor's name

--- src/draw.lua
+++ src/draw.lua
@@ -6,8 +6,16 @@ function draw.draw_matrix(tilematrix,tileset,quads)
   end
 end
 
+
+-- Actors
 function draw.draw_actor(act,sprite)
   full_quad = love.graphics.newQuad(0, 0, 16, 16, sprite:getDimensions())
   love.graphics.draw(sprite, full_quad, act.x * 16, act.y * 16)
 end
+
+function draw.draw_actor_name(act)
+  love.graphics.print(act.name, act.x*16, act.y*16 -16)
+end
+
+
 return draw
