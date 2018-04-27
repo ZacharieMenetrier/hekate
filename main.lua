6e618c1345cb9bd3eca047e1ba4b30a03010b3cc
Quentin FERRE <quentin.ferre@laposte.net>
2018-04-27 03:11:54 +0200

    Some variables are now properly loaded in love.load

--- main.lua
+++ main.lua
@@ -6,11 +6,24 @@ local chunk = matrix.read_matrix("test", "chunk")
 local draw = require "src/draw"
 local actor = require "src/actor"
 
+
+
+function love.load()
+  --tilematrix = matrix.iter_matrix(chunk) -- TODO find out why this one cannot be loaded in love.load
+  actorslist = actor.read_actors('dummy', 'actors')
+end
+
 function love.draw()
-  tilematrix = matrix.iter_matrix(chunk)
+  -- Matrix test
+  tilematrix = matrix.iter_matrix(chunk) -- placeholder while we figure out why it cannot be loaded in love.load
   draw.draw_matrix(tilematrix,loader.tileset,quads)
   love.graphics.print(love.timer.getFPS(), 10, 10)
 
-  dummy_actor = actor.create_actor('dummy',4,4)
-  draw.draw_actor(dummy_actor,loader.sprite)
+  -- Actors test
+  for i = 1, #actorslist do
+    draw.draw_actor(actorslist[i],loader.sprite)
+    draw.draw_actor_name(actorslist[i])
+  end
+
+end
 end
