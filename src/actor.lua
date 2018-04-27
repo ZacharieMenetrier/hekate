d2804f2e93cac2beb84a60f393b98ac6662fe75e
Quentin FERRE <quentin.ferre@laposte.net>
2018-04-27 03:11:06 +0200

    List of actors can now be read from a text file

--- src/actor.lua
+++ src/actor.lua
@@ -1,8 +1,35 @@
+utils = require "src/utils"
+
 local actor = {}
 
 function actor.create_actor(myname,myx,myy)
   act = {name = myname, x = myx, y = myy}
   return act
 end
+
+
+-- Read actor list
+function actor.read_actors(name, type)
+  local file_path = type .. "/" .. name .. "." .. type
+  local str_actors = utils.read_file(file_path)
+  return actor.str_to_actorslist(str_actors)
+end
+
+function actor.str_to_actorslist(str_actors)
+  local actlist = {}
+  i=1
+  for line in str_actors:gmatch("[^\n]+") do
+
+    args = {}
+    for cell in line:gmatch("%w+") do
+      table.insert(args, cell)
+    end
+
+    actlist[i] = actor.create_actor(unpack(args))
+    i = i + 1
+  end
+  return actlist
+end
+
 
 return actor
