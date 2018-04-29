8d9eb9bfc1cef05131e2f22e9c1a19107e139d35
System Manager <quentin.ferre@laposte.net>
2018-04-29 18:36:47 +0200

    The controller now has control of the action queue

--- src/controller.lua
+++ src/controller.lua
@@ -1,18 +1,35 @@
 local controller = {}
 
--- The controller has access to queue_action and will add new actions to it when
--- its functions are called.
 
-function controller.update(entity)
-  print(queue_action)
-  local team = entity.team
-  if team == "computer" then
-    local action = function()
-      entity.position.y = entity.position.y + 1
-    end
-    queue_action(action)
-  end
+
+-- The controller manages the action queue
+local queue = require "src/utils/queue"
+local action_queue = queue.new()
+
+function controller.queue_action(new_action)
+  -- For now the controller automatically pushes actions, later it will perform checks
+  queue.push_action_last(action_queue, new_action)
 end
+
+function controller.get_pending_action()
+  return queue.pop_first_action(action_queue)
+end
+
+
+--
+-- function controller.update(entity)
+--   local team = entity.team
+--   if team == "computer" then
+--     local action = function()
+--       entity.position.y = entity.position.y + 1
+--     end
+--     queue_action(action)
+--   end
+-- end
+
+
+-------------------------- BELOW : dealing with human input --------------------
+
 
 function controller.keypressed(entity, key)
   local team = entity.team
@@ -21,7 +38,7 @@ function controller.keypressed(entity, key)
       local action = function()
         entity.position.x = entity.position.x + 1
       end
-      queue_action(action)
+      controller.queue_action(action)
     end
   end
 end
