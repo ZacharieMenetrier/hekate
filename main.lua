7f4729d02c9901858f9931235e72247e519daeec
Zacharie Menetrier <zacharie.menetrier@gmail.com>
2018-05-01 12:00:21 +0200

    Gamestate is in controller merge conflicts

--- main.lua
+++ main.lua
@@ -5,12 +5,10 @@ controller:load_gamestate()
 
 function love.update(dt)
   -- Does the current entity have actions left ?
-  local action_left = controller.get_action_left(gamestate.entity)
+  local action_left = controller:get_action_left()
   -- If not, move on to the next entity
   if action_left == 0 then
-    controller.call_entity("end", gamestate.entity, gamestate, resource)
-    gamestate:next_turn()
-    controller.call_entity("begin", gamestate.entity, gamestate, resource)
+    controller:next_turn()
   -- Otherwise, tell the entity to call its "run" component
   else
     controller:call_current("run")
@@ -22,11 +20,14 @@ function love.keypressed(key, scancode, isrepeat)
 end
 
 function love.draw()
+  -- Draw the tilemap
   graphics.draw_tileset(controller.gamestate.tilemap, controller.resource.tileset.ascii)
   local entity = controller.gamestate.entity
   local xpix = entity.position.x * graphics.tile_size + graphics.tile_size / 2
   local ypix = entity.position.y * graphics.tile_size + graphics.tile_size / 2
+  -- Draw a circle around the current entity
   love.graphics.circle("fill", xpix, ypix, graphics.tile_size * 0.66)
+  -- Draw the entities
   for _, entity in ipairs(controller.gamestate.cluster) do
     local sprite = controller.resource.sprite["pig"]
     local xpix = entity.position.x * graphics.tile_size
--- src/controller.lua
+++ src/controller.lua
@@ -1,7 +1,7 @@
 local controller = {gamestate = require "src/gamestate",
                     resource = require "src/resource"}
 
-<<<<<<< HEAD
+
 function controller:load_gamestate()
   self.gamestate:load_gamestate()
 end
@@ -12,15 +12,10 @@ function controller:next_turn()
   self:call_cluster("begin")
 end
 
+-- Call a given entity
 function controller:call_entity(call, entity, ...)
   for component_name, _ in pairs(self.gamestate.entity) do
     local component = self.resource.component[component_name]
-=======
--- Call a given entity
-function controller.call_entity(call, entity, gamestate, resource, ...)
-  for component_name, _ in pairs(entity) do
-    local component = resource.component[component_name]
->>>>>>> 6f765063e3883217c0f3f81f42b41d5bc8773d87
     if component ~= nil then
       if component[call] ~= nil then
         component[call](entity, self, ...)
@@ -29,31 +24,21 @@ function controller.call_entity(call, entity, gamestate, resource, ...)
   end
 end
 
-<<<<<<< HEAD
+
 function controller:call_current(call, ...)
   self:call_entity(call, self.gamestate.entity, ...)
 end
 
+-- Call all entites in cluster
 function controller:call_cluster(call, ...)
   for _, entity in pairs(self.gamestate.cluster) do
     self:call_entity(call, entity, ...)
   end
 end
 
+-- Does this entity have any actions left ?
 function controller:get_action_left()
   local action = self.gamestate.entity.action
-=======
--- Call all entites in cluster
-function controller.call_cluster(call, gamestate, resource, ...)
-  for _, entity in pairs(gamestate.cluster) do
-    controller.call_entity(call, entity, gamestate, resource, ...)
-  end
-end
-
--- Does this entity have any actions left ?
-function controller.get_action_left(entity)
-  local action = entity.action
->>>>>>> 6f765063e3883217c0f3f81f42b41d5bc8773d87
   if not action then return 0 end
   return action.left
 end
