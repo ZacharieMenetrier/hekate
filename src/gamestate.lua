fd8eeeefd2b2763aa34d6d90864211e8a209406b
Zacharie Menetrier <zacharie.menetrier@gmail.com>
2018-05-01 10:01:16 +0200

    Cleaned gamestate in a module

---
+++ src/gamestate.lua
@@ -0,0 +1,24 @@
+local world = require "src/world"
+local utils = require "src/utils/utils"
+
+local gamestate = {}
+
+function gamestate:load_gamestate()
+  local save = utils.read_table("data/save")
+  local tilemap, cluster = world.read_world(save.world)
+  self.tilemap = tilemap
+  self.cluster = cluster
+  self.turn_pointer = save.turn_pointer
+  self.entity = self.cluster[self.turn_pointer]
+  assert(self.entity, "No entities when loading the save.")
+end
+
+function gamestate:next_turn()
+  self.turn_pointer = self.turn_pointer + 1
+  if self.cluster[self.turn_pointer] == nil then
+    self.turn_pointer = 1 end
+  self.entity = self.cluster[self.turn_pointer]
+  assert(self.entity, "No more entities in the world.")
+end
+
+return gamestate
