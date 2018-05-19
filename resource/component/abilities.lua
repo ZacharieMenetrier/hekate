ac5d9680e469a9bc6be659ee8d7791c9b5a302a7
Quentin FERRE <quentin.ferre@laposte.net>
2018-05-19 17:00:31 +0200

    Trying to call an unlearned ability will no longer crash the game

--- resource/component/abilities.lua
+++ resource/component/abilities.lua
@@ -8,18 +8,17 @@ local Abilities = Component:new()
 
 -- @brief Learn an ability
 function Abilities:learn_ability(ability_name)
-  -- Query the ability file
   ability_to_learn = require ("resource/ability/" .. ability_name)
-
   self.abilities[ability_name] = ability_to_learn
   print("You have learned : " .. ability_name) -- Debug
 end
 
--- @brief Query an ability and call it
+-- @brief Get an ability from the list of known ones, and call it
 function Abilities:call_ability(ability_name,parameters)
-  local ability_to_call = self.abilities[ability_name]
-  -- Call the ability.
-  ability_to_call(self,unpack(parameters))
+  local ability_to_call = nil
+  ability_to_call = self.abilities[ability_name]
+  if (ability_to_call == nil) then print("Ability not yet learned.")
+  else ability_to_call(self,unpack(parameters)) end
 end
 
 
