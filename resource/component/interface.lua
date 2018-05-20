dcc70490bd4981ef2829e03ba396a90e39e6788f
Quentin FERRE <quentin.ferre@laposte.net>
2018-05-20 22:30:24 +0200

    Interface will now also display abilities

--- resource/component/interface.lua
+++ resource/component/interface.lua
@@ -20,13 +20,13 @@ function Interface:mousepressed(x, y, button)
     clicked_y_tile = math.floor((y + camera.y) / (graphics.tile_size * graphics.scale))
 
 
-    -- TODO Separate entity selection and tile selection
+    -- TODO Separate entity selection and tile selection, with tile selection as a default
     print("X = " .. clicked_x_tile .. " , Y = ".. clicked_y_tile)
     filter_pos = function(c) if (c.__name == "position" and c.x == clicked_x_tile and c.y == clicked_y_tile) then return true end end
 
     -- Get the position component that has this tile
     position_on_tile = world.select(filter_pos)
-    for i,c in pairs(position_on_tile) do self.selected_entity = c.__entity end
+    for _,c in pairs(position_on_tile) do self.selected_entity = c.__entity end
 
     -- TODO Add a keybinding to unselect
 
@@ -43,7 +43,9 @@ function Interface:draw_ui()
   if self.selected_entity ~= nil then
     if world.exists(self.selected_entity, "vitals") then
       local vitals = world.get(self.selected_entity, "vitals")
-      self.draw_ui_vitals(vitals,posx,posy)
+      local abilities = world.get(self.selected_entity, "abilities")
+      self.draw_ui_vitals(vitals,100,100)
+      self.draw_ui_abilties(abilities,100,300)
     end
   end
 
@@ -55,14 +57,22 @@ end
 
 function Interface.draw_ui_vitals(vitals,posx,posy)
   if vitals ~= nil then
-    love.graphics.print("---- ".. vitals.name .. " ----",100,80)
-    love.graphics.print("Health : ".. vitals.hp,100,100)
-    love.graphics.print("Armor : ".. vitals.armor,100,120)
-    love.graphics.print("R : ".. vitals.red_wp,100,140)
-    love.graphics.print("G : ".. vitals.green_wp,150,140)
-    love.graphics.print("B : ".. vitals.blue_wp,200,140)
+    love.graphics.print("---- ".. vitals.name .. " ----",posx,posy)
+    love.graphics.print("Health : ".. vitals.hp,posx,posy+20)
+    love.graphics.print("Armor : ".. vitals.armor,posx,posy+40)
+    love.graphics.print("R : ".. vitals.red_wp,posx,posy+60)
+    love.graphics.print("G : ".. vitals.green_wp,posx+50,posy+60)
+    love.graphics.print("B : ".. vitals.blue_wp,posx+100,posy+60)
   end
 end
 
+function Interface.draw_ui_abilties(abilities,posx,posy)
+  love.graphics.print("-- Abilities -- ",posx,posy-20)
+  local posdelta = 0
+  for a,_ in pairs(abilities.abilities) do
+    love.graphics.print(a,posx,posy+posdelta)
+    posdelta = posdelta + 20
+  end
+end
 
 return Interface
