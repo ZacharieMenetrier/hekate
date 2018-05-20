c4e056ef13cb32c856006cd82a6d6e84b2a72249
Zacharie Menetrier <zacharie.menetrier@gmail.com>
2018-05-20 11:17:15 +0200

    Cleaned tilemap drawing

--- resource/component/tilemap.lua
+++ resource/component/tilemap.lua
@@ -9,25 +9,30 @@ local math = require "math"
 --private variables-------------------------------------------------------------
 --------------------------------------------------------------------------------
 
-local draw_quad = function(xtile, ytile, xquad, yquad, tile, tileset, tilemap)
-  local xpix = xtile * graphics.tile_size
-  local ypix = ytile * graphics.tile_size
-  xpix = xpix + ((xquad + 1) / 2) * graphics.quad_size
-  ypix = ypix + ((yquad + 1) / 2) * graphics.quad_size
-  local h = tilemap:get_tile(xtile + xquad, ytile) == tile
-  local v = tilemap:get_tile(xtile, ytile + yquad) == tile
-  local d = tilemap:get_tile(xtile + xquad, ytile + yquad) == tile
-  local seed = xtile * ytile + xquad * yquad
-  local quad = graphics.get_tileset_quad(tile, xquad, yquad, h, v, d, seed)
+--! @brief Draw a quad of the tilemap.
+local draw_quad = function(x, y, xq, yq, tile, tileset, tilemap, xpix, ypix, xi, yi)
+  -- Get the horizontal neighbour.
+  local h = tilemap:get_tile(x + xq, y) == tile
+  -- Get the vertical neighbour.
+  local v = tilemap:get_tile(x, y + yq) == tile
+  -- Get the diagonal neighbour.
+  local d = tilemap:get_tile(x + xq, y + yq) == tile
+  -- Get the corresponding quad.
+  local quad = graphics.get_tileset_quad(tile, xi, yi, h, v, d)
   love.graphics.draw(tileset, quad, xpix, ypix)
 end
 
-local draw_tile = function(xtile, ytile, tile, tileset, tilemap)
-  for y = -1, 1, 2 do
-    for x = -1, 1, 2 do
-      draw_quad(xtile, ytile, x, y, tile, tileset, tilemap)
-    end
-  end
+--! @brief Draw a tile of the tilemap.
+local draw_tile = function(x, y, tile, tileset, tilemap)
+  local xpix = x * graphics.tile_size
+  local ypix = y * graphics.tile_size
+  -- The quad size will be used to shift the x and y in the quads.
+  local qs = graphics.quad_size
+  -- Draw the four quads of the tile.
+  draw_quad(x, y, -1, -1, tile, tileset, tilemap, xpix, ypix, 0, 0)
+  draw_quad(x, y, -1, 1, tile, tileset, tilemap, xpix, ypix + qs, 0, 1)
+  draw_quad(x, y, 1, -1, tile, tileset, tilemap, xpix + qs, ypix, 1, 0)
+  draw_quad(x, y, 1, 1, tile, tileset, tilemap, xpix + qs, ypix + qs, 1, 1)
 end
 
 --------------------------------------------------------------------------------
@@ -43,14 +48,12 @@ end
 function Tilemap:draw_tilemap()
   local camera = world.get("system", "camera")
   local tileset = resource.get("tileset", self.tileset)
+  -- Get the position of all visible tiles.
   local tiles = camera:tiles_visibles()
-  for _, tile in ipairs(tiles) do
-    local x = tile.x
-    local y = tile.y
-    tile = self:get_tile(x, y)
-    if tile then
-      draw_tile(x, y, tile, tileset, self)
-    end
+  -- Draw only visible tiles.
+  for _, tile_position in ipairs(tiles) do
+    local tile = self:get_tile(tile_position.x, tile_position.y)
+    draw_tile(tile_position.x, tile_position.y, tile, tileset, self)
   end
 end
 
--- src/graphics.lua
+++ src/graphics.lua
@@ -1,22 +1,30 @@
+-- public
+local scale = 2
+-- public
+local tile_size = 32
+-- public
+local quad_size = 16
+
 --------------------------------------------------------------------------------
---private variables--------------------------------------------------------------
+--private variables-------------------------------------------------------------
 --------------------------------------------------------------------------------
 
-local scale = 2
-local tile_size = 32
 local quads_per_tileset = 16
-local tileset_size = 256
-local quad_size = 16
 local tileset_quads = {}
 
-
-local nq = love.graphics.newQuad
-for y = 0, quads_per_tileset - 1 do
-  for x = 0, quads_per_tileset - 1 do
-    local xpx = x * quad_size
-    local ypx = y * quad_size
-    local quad = nq(xpx, ypx, quad_size, quad_size, tileset_size, tileset_size)
-    tileset_quads[x + y * quads_per_tileset] = quad
+-- Calculate the quads for the tileset.
+do
+  local tileset_size = 256
+  local qs = quad_size
+  local ts = tileset_size
+  local nq = love.graphics.newQuad
+  for y = 0, quads_per_tileset - 1 do
+    for x = 0, quads_per_tileset - 1 do
+      local xpx = x * qs
+      local ypx = y * qs
+      local quad = nq(xpx, ypx, qs, qs, ts, ts)
+      tileset_quads[x + y * quads_per_tileset] = quad
+    end
   end
 end
 
@@ -24,13 +32,12 @@ end
 --public variables--------------------------------------------------------------
 --------------------------------------------------------------------------------
 
-local get_tileset_quad = function(tile, xq, yq, h, v, d, seed)
-  local y = tile * 2
-  local x = (xq + 1) / 2
-  local y = y + (yq + 1) / 2
+local get_tileset_quad = function(tile, x, y, h, v, d)
+  y = y + tile * 2
   local shift = 0
-  if h and v and d then shift = 4 end
-  if h and v and not d then shift = 3 end
+  if h and v then
+    if d then shift = 4 else shift = 3 end
+  end
   if h and not v then shift = 2 end
   if v and not h then shift = 1 end
   return tileset_quads[x + shift * 2 + y * quads_per_tileset]
